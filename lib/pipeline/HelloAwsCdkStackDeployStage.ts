81159d35f07546504eaae108a3f11807b536cdb2
Raku Zeta <zetaraku@gmail.com>
2023-03-11 23:57:19 +0800

    ✨ expose stack from HelloAwsCdkStackDeployStage

--- lib/pipeline/HelloAwsCdkStackDeployStage.ts
+++ lib/pipeline/HelloAwsCdkStackDeployStage.ts
@@ -3,9 +3,16 @@ import { Construct } from 'constructs';
 import { HelloAwsCdkStack } from '../HelloAwsCdkStack';
 
 export class HelloAwsCdkStackDeployStage extends CDK.Stage {
+  /**
+   * The `HelloAwsCdkStack` stack to deploy.
+   */
+  public readonly stack: HelloAwsCdkStack;
+
   constructor(scope: Construct, id: string, props?: CDK.StageProps) {
     super(scope, id, props);
 
     const stack = new HelloAwsCdkStack(this, 'HelloAwsCdkStack');
+
+    this.stack = stack;
   }
 }
