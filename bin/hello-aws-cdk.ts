bdb4c4bb99fa630817c365d2396b456ea12be854
Raku Zeta <zetaraku@gmail.com>
2023-03-11 23:39:28 +0800

    ✨ use HelloAwsCdkPipelineStack in hello-aws-cdk

--- bin/hello-aws-cdk.ts
+++ bin/hello-aws-cdk.ts
@@ -2,6 +2,8 @@
 import 'source-map-support/register';
 import * as CDK from 'aws-cdk-lib';
 import { HelloAwsCdkStack } from '../lib/HelloAwsCdkStack';
+import { HelloAwsCdkPipelineStack } from '../lib/pipeline/HelloAwsCdkPipelineStack';
 
 const app = new CDK.App();
 const stack = new HelloAwsCdkStack(app, 'HelloAwsCdkStack');
+const pipeline = new HelloAwsCdkPipelineStack(app, 'HelloAwsCdkPipelineStack');
