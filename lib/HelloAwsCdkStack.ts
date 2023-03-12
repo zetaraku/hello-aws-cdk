cc08dbc678a41adfb95d029b60da0c892d827b3e
Raku Zeta <zetaraku@gmail.com>
2023-03-12 12:07:44 +0800

    ✨ use hello lambda function

--- lib/HelloAwsCdkStack.ts
+++ lib/HelloAwsCdkStack.ts
@@ -4,5 +4,11 @@ import { Construct } from 'constructs';
 export class HelloAwsCdkStack extends CDK.Stack {
   constructor(scope: Construct, id: string, props?: CDK.StackProps) {
     super(scope, id, props);
+
+    const helloFunction = new CDK.aws_lambda_nodejs.NodejsFunction(this, 'HelloFunction', {
+      runtime: CDK.aws_lambda.Runtime.NODEJS_18_X,
+      entry: 'lambda/hello.ts',
+      handler: 'handler',
+    });
   }
 }
